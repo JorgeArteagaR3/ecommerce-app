38344b95fffdfd81fb3db108c2469695fe70f52c
Jorge Arteaga Aranibar <jorgeaar3al@gmail.com>
2022-08-06 23:32:15 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <nav>
+                    <h1>MasterMind</h1>
+                    <ul>
+                        <li>Man</li>
+                        <li>Women</li>
+                        <li>Kids</li>
+                        <li>Accesories</li>
+                    </ul>
+                    <div></div>
+                </nav>
+            </header>
+        </div>
+    );
 }
 
 export default App;
